18d4962c3f3d6b5948cd603cdb2278171732a274
Anonymous Maarten <madebr@users.noreply.github.com>
2020-12-27 15:14:11 +0100

    Fix typo in markdown message (+ reword it a bit)

--- main.go
+++ main.go
@@ -81,7 +81,7 @@ func main() {
 
 ### :ballot_box_with_check: Selection Criteria:
 
-- No reivews and pull requests was created within 24hrs
+- No reviews or pull requests were created in the last 24hrs
 - There has been at least one approval
 
 ### :nerd_face: Please Review!
