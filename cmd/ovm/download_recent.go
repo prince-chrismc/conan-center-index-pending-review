c4ed49a0a5fc409f5b29abee29db1ebefa0dc144
Chris Mc <prince.chrismc@gmail.com>
2022-02-06 20:44:07 -0800

    Open versus merged GIT to use a fixed number of images (#38)
    switching from time to fix number of frames to use when creating the GIF
    
    resolves #36 the number should always be less then 1 MB since everything is fixed sized
    resolves #37 there's already 100s of commits so the fixed number is less... there's no temporal factor to break things

--- cmd/ovm/download_recent.go
+++ cmd/ovm/download_recent.go
@@ -5,22 +5,20 @@ import (
 	"image"
 	"image/png"
 	"strings"
-	"time"
 
 	"github.com/prince-chrismc/conan-center-index-pending-review/v2/internal"
-	"github.com/prince-chrismc/conan-center-index-pending-review/v2/internal/duration"
 	"github.com/prince-chrismc/conan-center-index-pending-review/v2/pkg/pending_review"
 )
 
+// GetOvmPngFromThisWeek returns the last sevent PNGs that have been uploaded.
+// Note: This is under the basis that the deployment does this every ~24hrs.
 func GetOvmPngFromThisWeek(context context.Context, client *pending_review.Client) ([]image.Image, error) {
-	window := time.Now().Truncate(duration.WEEK)
-
-	commits, err := internal.GetCommitsSince(context, client, "open-versus-merged.png", window)
+	commits, err := internal.GetCommits(context, client, "open-versus-merged.png", 7)
 	if err != nil {
 		return nil, err
 	}
 
-	snapshots := make([]image.Image, 0)
+	snapshots := make([]image.Image, 0, 7)
 
 	for _, commit := range commits {
 		fileContent, err := internal.GetDataFileAtRef(context, client, "open-versus-merged.png", commit.GetSHA())
