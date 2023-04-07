4b59d2c609e5e6650fe2885987b1a83a210a9132
Christopher McArthur <christopherm@jfrog.com>
2023-04-07 12:57:05 -0700

    move process label

--- pending_review/pull_request.go
+++ pending_review/pull_request.go
@@ -176,18 +176,3 @@ func (s *PullRequestService) determineTypeOfChange(ctx context.Context, owner st
 
 	return change, resp, nil
 }
-
-func processLabels(labels []*Label) error {
-	for _, label := range labels {
-		switch label.GetName() {
-		case "bug", "stale", "Failed", "infrastructure", "blocked", "duplicate", "invalid":
-			return fmt.Errorf("%w", ErrStoppedLabel)
-		case "Bump version", "Bump dependencies":
-			return fmt.Errorf("%w", ErrBumpLabel)
-		default:
-			continue
-		}
-	}
-
-	return nil
-}
